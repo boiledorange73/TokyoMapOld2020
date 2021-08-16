cdd19361eda06aeacc7d0601ee65f2d17699c4ed
boiledorange73 <boiledorange73@gmail.com>
2021-08-16 15:54:46 +0900

    URL parameter lc supports

--- app/src/main/assets/js/resources.js
+++ app/src/main/assets/js/resources.js
@@ -3,6 +3,20 @@ if( window.BO == null ) {
 }
 
 BO.lang = function() {
+  // supports "lc" URL param.
+  var search = window.location && window.location.search ? window.location.search : "";
+  if( search.substr(0,1) == "?" ) {
+    search = search.substr(1);
+  }
+  var aparams = search.length > 0 ? search.split("&") : null;
+  var len = aparams ? aparams.length : 0;
+  for( var n = 0; n < len; n++ ) {
+    var param = aparams[n];
+    if( param != null && param.substr(0,3) == "lc=" ) {
+      return param.substr(3);
+    }
+  }
+  // Gets the language from navigator.
   if( navigator != null && navigator.language != null ) {
     var arr = navigator.language.split("-");
     if( arr != null && arr.length > 0 ) {
